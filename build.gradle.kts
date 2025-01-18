d09fcb6fc4ceae9c6708d931f41c8d2688164489
MZ <abdullah0306533@gmail.com>
2025-01-18 19:48:23 +0500

    Added all required dependencies permissions etc..

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+
+
+    //for migration of kapt to ksp
+    id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
+    //Hilt
+    id ("com.google.dagger.hilt.android") version "2.53.1" apply false
+}
